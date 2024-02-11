e5b606856db733a6c3b315ac2f092281689e4f0f
Justin Pawlowski <justinpawlowski@Justins-Air.attlocal.net>
2024-02-11 15:38:45 -0600

    Documentation

--- VacuumWorld/RandomizerButton.java
+++ VacuumWorld/RandomizerButton.java
@@ -2,15 +2,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.List;
 
 /**
- * Write a description of class RandomizerButton here.
+ * Randomizes which rooms are dirty
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Matt Hansen, Justin Pawlowski
+ * @version 2/11/2024
  */
 public class RandomizerButton extends Button
 {
      void onClick(){
+         // List of the rooms in the world
          List<Room> roomList = getWorld().getObjects(Room.class);
+         // 50% chance that a room has dirt
          for (Room room : roomList){
              int num = Greenfoot.getRandomNumber(2);
              if (num == 0){
--- VacuumWorld/StartButton.java
+++ VacuumWorld/StartButton.java
@@ -1,13 +1,14 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class StartButton here.
+ * To start the vacuum
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Matt Hansen, Justin Pawlowski
+ * @version 2/11/2024
  */
 public class StartButton extends Button
 {
+    // Set start to true, start the vacuum
      void onClick(){
         ((MyWorld)this.getWorld()).setStarted(true);
     }
