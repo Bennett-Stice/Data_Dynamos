23697da0e5a74f1ced08088255e576ddf20b21e4
AuburnCampfire <mahansen2002@gmail.com>
2024-02-11 14:16:30 -0600

    Moved some changes from Gavin's upload and modified dirt placement to be a toggle

--- VacuumWorld/Room.java
+++ VacuumWorld/Room.java
@@ -8,12 +8,41 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Room extends Actor
 {
+    private boolean hasDirt = false;
+    private Dirt thisRoomsDirt;
     /**
      * Act - do whatever the Room wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.mouseClicked(this)){
+            toggleDirt();
+        }
+    }
+    
+    
+    public void addDirt(){
+        if(!hasDirt){
+            int xLoc=this.getX();
+            int yLoc=this.getY();
+            thisRoomsDirt = new Dirt();
+            getWorld().addObject (thisRoomsDirt,xLoc, yLoc);
+            hasDirt = true;
+        }
+    }
+    
+    public void removeDirt(){
+        if(hasDirt){
+            getWorld().removeObject(thisRoomsDirt);
+            hasDirt = false;
+        }
+    }
+    
+    public void toggleDirt(){
+        if(hasDirt)
+            removeDirt();
+        else
+            addDirt();
     }
 }
