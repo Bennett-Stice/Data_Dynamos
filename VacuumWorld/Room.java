7fb080e29f4577958c53933c5c70cdcd99524ec4
AuburnCampfire <mahansen2002@gmail.com>
2024-05-15 19:48:43 -0500

    Added main world class and supporting actors

---
+++ VacuumWorld/Room.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Room collectable. Adds to balloon score on pickup.
+ * 
+ * @author Matthew Hansen
+ * @version 10/26/21
+ */
+public class Room extends Actor
+{
+    /**
+     * Act - do whatever the Room wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
